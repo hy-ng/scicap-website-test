3a5cc029126831ef446051d32ae3c1330662c1af
Sam NG <samnghoyin@gmail.com>
2025-06-05 02:08:02 -0400

    adjust the background pattern

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,22 @@ import NewsSection from "./components/NewsSection";
 import DatasetSection from "./components/DatasetSection";
 import Footer from "./components/Footer";
 import "./App.css";
-// import BackgroundPattern from "./components/BackgroundPattern"; // Uncomment if you add it
+import BackgroundPattern from "./components/BackgroundPattern"; // Uncomment if you add it
 
 function App() {
   return (
-    <div>
-      {/* <BackgroundPattern /> */}
-      <Navbar />
-      <main>
-        <NewsSection />
-        <HeroSection />
-        <DatasetSection />
-      </main>
+    <>
+    <Navbar />
+      <div>
+      <BackgroundPattern />
+        <main>
+          <NewsSection />
+          <HeroSection />
+          <DatasetSection />
+        </main>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
 
---
+++ src/components/BackgroundPattern.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./BackgroundPattern.css";
+import pattern from "../assets/pattern-bg-v2.svg"; // Or paste SVG inline
+
+export default function BackgroundPattern() {
+  return (
+    <img
+      src={pattern}
+      className="background-pattern"
+      alt=""
+      aria-hidden="true"
+      draggable="false"
+    />
+  );
+}
