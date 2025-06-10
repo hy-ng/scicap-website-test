ff004aedef319dc3b2cf3ac9125eed1d3a6bd5a7
Sam NG <samnghoyin@gmail.com>
2025-06-10 16:41:07 -0400

    Modify the domain pointing

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://www.scicap.ai/',
+  base: 'https://scicap.ai/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
